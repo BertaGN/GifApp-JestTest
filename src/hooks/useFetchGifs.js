a88e5ff5e9a808d9b36c61d4daa25ad496e55551
Berta Garcia <bertagnavarro93@gmail.com>
2023-06-07 16:08:32 +0200

    exercise practising custom hooks

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,27 @@
+
+//un hook es una funcion que retorna un objeto
+
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+        setIsLoading(false);
+    }
+
+
+    useEffect(() => {
+        getImages();
+    }, [])
+
+
+    return {
+        images,
+        isLoading
+    }
+}
