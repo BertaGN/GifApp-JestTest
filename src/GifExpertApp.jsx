742cb93a0a6a0ef08a9d7612414cddd3fb866d63
Berta Garcia <bertagnavarro93@gmail.com>
2023-06-12 15:16:58 +0200

    jest test added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
+
 
 
 export const GifExpertApp = () => {
@@ -16,7 +16,6 @@ export const GifExpertApp = () => {
     return (
         <>
             <h1>GifExpertApp</h1>
-            {/* Input en componente a parte */}
             <AddCategory
                 onNewCategory={(value) => onAddCategory(value)}
             />
